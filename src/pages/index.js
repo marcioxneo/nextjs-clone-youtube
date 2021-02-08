0d11379607dd253ff5a2e82d7b8d3675fc9806d5
Marcio Andrade <marcioandr@live.com>
2021-02-08 18:37:24 -0300

    Material-UI confuguration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone youtube com Next.js e Material+UI</div>;
+  return (
+    <div>
+      Clone youtube com Next.js e Material+UI
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
 }
