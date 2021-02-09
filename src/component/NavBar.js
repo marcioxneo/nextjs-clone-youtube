89880cccce7f18ee0df4cea09688b2caca651057
Marcio Andrade <marcioandr@live.com>
2021-02-09 03:06:52 -0300

    Home Page

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -21,6 +21,20 @@ import Whatshot from '@material-ui/icons/Whatshot';
 import VideoLibrary from '@material-ui/icons/VideoLibrary';
 import History from '@material-ui/icons/History';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import MusicVideo from '@material-ui/icons/MusicVideo';
+import SportsSoccer from '@material-ui/icons/SportsSoccer';
+import SportsEsports from '@material-ui/icons/SportsEsports';
+import Movie from '@material-ui/icons/Movie';
+import ViewStream from '@material-ui/icons/ViewStream';
+import Receipt from '@material-ui/icons/Receipt';
+import EmojiObjects from '@material-ui/icons/EmojiObjects';
+import YouTube from '@material-ui/icons/YouTube';
+import Theaters from '@material-ui/icons/Theaters';
+import Settings from '@material-ui/icons/Settings';
+import Flag from '@material-ui/icons/Flag';
+import Help from '@material-ui/icons/Help';
+import Feedback from '@material-ui/icons/Feedback';
+import AddCircle from '@material-ui/icons/AddCircle';
 
 const useStyles = makeStyles((theme) => ({
   mobileDrawer: {
@@ -59,8 +73,31 @@ const primaryMenu = [
 ];
 
 const secondaryMenu = [
-  { id: 1, label: 'Biblioteca', icon: VideoLibrary },
-  { id: 2, label: 'Histórico', icon: History },
+  { id: 1, label: 'Biblioteca', path: 'library', icon: VideoLibrary },
+  { id: 2, label: 'Histórico', path: 'history', icon: History },
+];
+
+const tertiaryMenu = [
+  { id: 1, label: 'Música', path: 'music', icon: MusicVideo },
+  { id: 2, label: 'Esportes', path: 'sports', icon: SportsSoccer },
+  { id: 3, label: 'Jogos', path: 'games', icon: SportsEsports },
+  { id: 4, label: 'Filmes', path: 'movies', icon: Movie },
+  { id: 5, label: 'Notícias', path: 'news', icon: Receipt },
+  { id: 6, label: 'Ao Vivo', path: 'live', icon: ViewStream },
+  { id: 7, label: 'Aprender', path: 'learn', icon: EmojiObjects },
+  { id: 8, label: 'Vídeos do Momento', path: 'spotligth', icon: YouTube },
+  { id: 9, label: 'Vídeo em 360°', path: '360', icon: Theaters },
+];
+
+const maisCanais = [
+  { id: 1, label: 'Procurar canais', path: 'more', icon: AddCircle },
+];
+
+const quatermasterMenu = [
+  { id: 1, label: 'Configuração', path: 'config', icon: Settings },
+  { id: 2, label: 'Histórico de Denúncias', path: 'denuncias', icon: Flag },
+  { id: 3, label: 'Ajuda', path: 'help', icon: Help },
+  { id: 4, label: 'Enviar feedback', path: 'feedback', icon: Feedback },
 ];
 
 function NavBar() {
@@ -132,6 +169,108 @@ function NavBar() {
             Fazer Login
           </Button>
         </Box>
+      </Box>
+      <Divider />
+      <Box mx={4} my={2}>
+        <Typography variant="subtitle2">
+          <b>O MELHOR DO YOUTUBE</b>
+        </Typography>
+      </Box>
+      <List>
+        {tertiaryMenu.map((item) => {
+          const Icon = item.icon;
+          return (
+            <ListItem
+              key={item.id}
+              button
+              classes={{ root: classes.listItem }}
+              selected={isSelected(item)}
+            >
+              <ListItemIcon>
+                <Icon style={{ color: isSelected(item) && '#f44336' }} />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText,
+                }}
+                primary={item.label}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
+      <Divider />
+      <List>
+        {maisCanais.map((item) => {
+          const Icon = item.icon;
+          return (
+            <ListItem
+              key={item.id}
+              button
+              classes={{ root: classes.listItem }}
+              selected={isSelected(item)}
+            >
+              <ListItemIcon>
+                <Icon style={{ color: isSelected(item) && '#f44336' }} />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText,
+                }}
+                primary={item.label}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
+      <Divider />
+      <Box mx={4} my={2}>
+        <Typography variant="subtitle2">
+          <b>MAIS DO YOUTUBE</b>
+        </Typography>
+      </Box>
+      <List>
+        {quatermasterMenu.map((item) => {
+          const Icon = item.icon;
+          return (
+            <ListItem
+              key={item.id}
+              button
+              classes={{ root: classes.listItem }}
+              selected={isSelected(item)}
+            >
+              <ListItemIcon>
+                <Icon style={{ color: isSelected(item) && '#f44336' }} />
+              </ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText,
+                }}
+                primary={item.label}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
+      <Divider />
+      <Box mx={4} my={2}>
+        <Typography variant="caption text">Sobre Imprensa</Typography>
+        <Typography variant="body2">Direitos autorais</Typography>
+        <Typography variant="body2">Entre em contato</Typography>
+        <Typography variant="body2">Criadores de conteúdo</Typography>
+        <Typography variant="body2">Publicidade Desenvolvedores</Typography>
+        <br />
+        <Typography variant="body2">Termos Privacidade</Typography>
+        <Typography variant="body2">Política e segurança</Typography>
+        <Typography variant="body2">Como funciona o YouTube</Typography>
+        <Typography variant="caption text">
+          Testar os nossos recursos
+        </Typography>
+        <br />
+        <br />
+        <Typography variant="body2">
+          <b>&copy; 2021 Google LLC</b>
+        </Typography>
       </Box>
     </Box>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,79 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-import Layout from '../component/Layout';
+import { Box, Grid } from '@material-ui/core';
 
-export default function Home() {
+import Layout from 'src/component/Layout';
+import VideoCard from 'src/component/VideoCard';
+
+function Home({ data }) {
   return (
     <Layout title="YouTube">
-      <div>
-        Clone youtube com Next.js e Material+UI
-        <Button variant="contained" color="primary">
-          Primary
-        </Button>
-      </div>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER [PARTE #01]',
+      authorId: 1,
+      authorName: 'Lucas Mhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title:
+        'NEXT.JS: ENTENDENDO A RENDERIZAÇÃO REACT NO LADO SERVIDOR [PARTE #02]',
+      authorId: 1,
+      authorName: 'Lucas Mhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'NEXT.JS: ROTAS ESTÁTISTICAS E DINÂMICAS [PARTE #03]',
+      authorId: 1,
+      authorName: 'Lucas Mhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 4,
+      title: 'NEXT.JS: ROTAS ESTÁTISTICAS E DINÂMICAS [PARTE #03]',
+      authorId: 1,
+      authorName: 'Lucas Mhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ];
+
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    },
+  };
+}
+
+export default Home;
